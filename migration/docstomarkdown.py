463e786fe198b6a83dd5d43ac07cd00f33db6e38
Sujeevan Vijayakumaran <mail@svij.org>
2018-03-08 16:56:20 +0100

    Continued work on docstomarkdown converter

--- migration/docstomarkdown.py
+++ migration/docstomarkdown.py
@@ -3,23 +3,27 @@
 
 from bs4 import BeautifulSoup
 import sys
-#reload(sys)
-#sys.setdefaultencoding("utf-8")
+import os
 
-f = open(sys.argv[1])
-c = f.read()
-s = BeautifulSoup(c, 'html.parser', from_encoding="utf-8")
-content = s.select("div.main")
-content = content[0]
+with os.scandir('/home/sujee/Repositories/tw.org/html/docs') as file_iterator:
+  for entry in file_iterator:
+    filename, filename_extension = os.path.splitext(entry)
+    if entry.is_file() and filename_extension == '.html':
+      print('Reading file: {}'.format(entry.path))
+      page = open(entry, 'rb')
+      raw_content = page.read()
+      bs = BeautifulSoup(raw_content, 'html.parser', from_encoding="utf-8")
+      content = bs.select("div.main")
+      content = content[0]
+#      title = content.find('h3').text
 
-title = content.find('h3').text
+      output_file = open(os.path.join('../content/page/docs', filename), 'w')
+      print(output_file)
+      output_file.write('---\n')
+#      output_file.write('title: {}'.format(title.strip()))
+#output_file.write('url: {}'.format(entry))
+      output_file.write('---\n')
+      output_file.close()
 
-content.find("h3").extract()
 
-path = "/docs/" + sys.argv[1]
-
-print("---")
-print("title: \"{}\"".format(title.strip()))
-print("url: '{}'".format(path))
-print("---")
-print(content.prettify())
+#print(content.prettify())
